dd7aaf704b57be15bf0cbb1ed86681d4f6846630
Nub4Ever <rbocanu@yahoo.com>
2023-08-17 15:54:50 +0300

    Finished Bank Account

--- src/main/java/org/example/bankaccount/Account.java
+++ src/main/java/org/example/bankaccount/Account.java
@@ -10,24 +10,24 @@ public class Account {
     private int nationalId;
 
     public void deposit(int amount) {
-        if(amount < 0) {
-            throw new InvalidAmountException("Cannot deposit this amount!");
+        if (amount < 0) {
+            throw new DepositAmountException("Invalid amount");
         }
 
-        if(this.amount + amount > Double.MAX_VALUE) {
-            throw new InvalidAmountException("Maximum possible amount value reached!");
+        if (this.amount + amount > Double.MAX_VALUE) {
+            throw new DepositAmountException("Amount overflow");
         }
 
         this.amount += amount;
     }
 
     public void withdraw(int amount) {
-        if(amount < 0) {
-            throw new InvalidAmountException("No money in the account!");
+        if (amount < 0) {
+            throw new DepositAmountException("Invalid amount");
         }
 
-        if(this.amount < amount) {
-            throw new NotEnoughMoneyException("Not enough money in the account!");
+        if (this.amount < amount) {
+            throw new NotEnoughMoneyException("Not enough money");
         }
 
         this.amount -= amount;

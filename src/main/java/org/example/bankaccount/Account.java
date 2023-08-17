7016dbfcdab8e5c9dd517fb6767f5739544aaabd
George-Valentin Cujbă <57318061+GeorgeValentin@users.noreply.github.com>
2023-08-17 15:58:16 +0300

    Finished Account Exercise (#1)

--- src/main/java/org/example/bankaccount/Account.java
+++ src/main/java/org/example/bankaccount/Account.java
@@ -10,11 +10,27 @@ public class Account {
     private int nationalId;
 
     public void deposit(int amount) {
-        // TODO implement
+        if(amount < 0) {
+            throw new InvalidAmountException("Cannot deposit this amount!");
+        }
+
+        if(this.amount + amount > Double.MAX_VALUE) {
+            throw new InvalidAmountException("Maximum possible amount value reached!");
+        }
+
+        this.amount += amount;
     }
 
     public void withdraw(int amount) {
-        // TODO implement
+        if(amount < 0) {
+            throw new InvalidAmountException("No money in the account!");
+        }
+
+        if(this.amount < amount) {
+            throw new NotEnoughMoneyException("Not enough money in the account!");
+        }
+
+        this.amount -= amount;
     }
 
     public void linkToNationalId(int nationalId) {
